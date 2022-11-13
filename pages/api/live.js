6b934eae3fa6cde5debb0f19df2d91fa492e1fdc
Neeraj Dalal <admin@nrjdalal.com>
2022-11-13 21:12:09 +0530

    minor ui update

--- pages/api/live.js
+++ pages/api/live.js
@@ -6,49 +6,47 @@ export default async function handler(req, res) {
 
   let stream = `https://jiotv.live.cdn.jio.com/${req.query.c}/${req.query.c}_1200.m3u8` + token(req.query.s)
 
-  try {
-    stream = await fetch(stream, {
-      headers: {
-        'User-Agent': 'plaYtv/6.0.9 (Linux; Android 5.1.1) ExoPlayerLib/2.13.2',
-      },
-    })
+  // try {
+  stream = await fetch(stream, {
+    headers: {
+      'User-Agent': 'plaYtv/6.0.9 (Linux; Android 5.1.1) ExoPlayerLib/2.13.2',
+    },
+  })
 
-    stream = await stream.text()
+  stream = await stream.text()
 
-    stream = stream.split('\n')
-    stream = stream.map((line) => {
-      if (line.endsWith('.ts')) {
-        return (
-          `https://jiotv.live.cdn.jio.com/${req.query.c}/${req.query.c}_1200` + line.substring(line.lastIndexOf('-'))
-        )
-      }
+  stream = stream.split('\n')
+  stream = stream.map((line) => {
+    if (line.endsWith('.ts')) {
+      return `https://jiotv.live.cdn.jio.com/${req.query.c}/${req.query.c}_1200` + line.substring(line.lastIndexOf('-'))
+    }
 
-      if (line.includes('URI=') && line.includes('IV=')) {
-        let a = line.substring(0, line.indexOf('URI='))
-        let b = line.substring(line.indexOf('URI='))
-        let key = b.substring(b.indexOf('-'), b.lastIndexOf('"'))
-        let c = b.substring(b.lastIndexOf('"'))
+    if (line.includes('URI=') && line.includes('IV=')) {
+      let a = line.substring(0, line.indexOf('URI='))
+      let b = line.substring(line.indexOf('URI='))
+      let key = b.substring(b.indexOf('-'), b.lastIndexOf('"'))
+      let c = b.substring(b.lastIndexOf('"'))
 
-        return (
-          a +
-          `URI="${protocol}//${host}/api/stream?key=${req.query.c}/${req.query.c}_1200${key}&s=${req.query.s}&u=${req.query.u}` +
-          c
-        )
-      }
+      return (
+        a +
+        `URI="${protocol}//${host}/api/stream?key=${req.query.c}/${req.query.c}_1200${key}&s=${req.query.s}&u=${req.query.u}` +
+        c
+      )
+    }
 
-      return line
-    })
+    return line
+  })
 
-    stream = stream.join('\n')
+  stream = stream.join('\n')
 
-    res.setHeader('Access-Control-Allow-Headers', 'Range')
-    res.setHeader('Accept-Range', 'bytes')
-    res.setHeader('Content-Type', 'application/vnd.apple.mpegurl')
+  res.setHeader('Access-Control-Allow-Headers', 'Range')
+  res.setHeader('Accept-Range', 'bytes')
+  res.setHeader('Content-Type', 'application/vnd.apple.mpegurl')
 
-    res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
-    res.status(200).send(stream)
-  } catch {
-    res.end()
-  }
+  res.status(200).send(stream)
+  // } catch {
+  //   res.end()
+  // }
 }
