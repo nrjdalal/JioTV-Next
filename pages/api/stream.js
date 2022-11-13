25541578f4dc328b9c7a9e0049316cb6e2212f21
Neeraj Dalal <admin@nrjdalal.com>
2022-11-13 21:48:25 +0530

    better stream test

--- pages/api/stream.js
+++ pages/api/stream.js
@@ -2,28 +2,28 @@ import { Readable } from 'stream'
 import { token } from '/utils/token'
 
 export default async function handler(req, res) {
-  // try {
-  const stream = await fetch('https://tv.media.jio.com/streams_live/' + req.query.key + token(req.query.s), {
-    headers: {
-      channelid: 'android',
-      deviceId: 'android',
-      devicetype: 'android',
-      os: 'android',
-      srno: 'android',
-      ssotoken: req.query.s,
-      uniqueId: req.query.u,
-      'User-Agent': 'android',
-      usergroup: 'android',
-      versionCode: 'android',
-    },
-  })
+  try {
+    const stream = await fetch('https://tv.media.jio.com/streams_live/' + req.query.key + token(req.query.s), {
+      headers: {
+        channelid: 'android',
+        deviceId: 'android',
+        devicetype: 'android',
+        os: 'android',
+        srno: 'android',
+        ssotoken: req.query.s,
+        uniqueId: req.query.u,
+        'User-Agent': 'android',
+        usergroup: 'android',
+        versionCode: 'android',
+      },
+    })
 
-  const output = Readable.from(await stream.buffer())
+    const output = Readable.from(await stream.buffer())
 
-  res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', '*')
 
-  res.status(200).send(output)
-  // } catch {
-  //   res.end()
-  // }
+    res.status(200).send(output)
+  } catch {
+    res.end()
+  }
 }
